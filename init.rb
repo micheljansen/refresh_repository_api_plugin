8b1923951c698a4ae8cbfbb7b717803739c166c1
Michel Jansen <mail@micheljansen.org>
2009-11-05 17:23:50 +0100

    bumped plugin version to 0.2.1

--- init.rb
+++ init.rb
@@ -13,6 +13,6 @@ Redmine::Plugin.register :refresh_repositories_api do
   url 'http://github.com/micheljansen/refresh_repository_api_plugin'
   author_url 'http://micheljansen.org'
   description "Adds a very simple API to invoke a refresh of a given repository remotely. Useful when using post commit hooks when the server running the SCM is not the same as the server running Redmine."
-  version '0.2.0'
+  version '0.2.1'
   requires_redmine :version_or_higher => '0.8.6'
 end
